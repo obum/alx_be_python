c963dfe313738663bee399e11db17758764c6c81
obum <obumanichebe@gmail.com>
2024-09-23 19:00:21 +0100

    updata: modified the arithmetic_operations.py file and included included main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1:float, num2:float, operation:str):
+    match operation:
+        
+        case 'add':
+            return num1 + num2
+        
+        case 'subtract':
+            return num1 - num2
+        
+        case 'multiply':
+            return num1 * num2
+        
+        case 'divide':
+            if num2  == 0:
+                return "num2 = 0, result is approaching infinity"
+            else:
+                return num1 / num2
+            
+        case _:
+            return f"unknow operation"
