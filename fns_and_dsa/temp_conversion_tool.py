92a8ced920d963a11835e4b86833fad01ff1300d
obum <obumanichebe@gmail.com>
2024-09-24 19:38:30 +0100

    updatae:  completed the code for the temp file

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,42 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+
+    celsius = (fahrenheit  - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
+    return celsius
+
+def convert_to_fahrenheit(celsius):
+
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    return fahrenheit
+
+user_temperature = input(f"Enter the temperature to convert: ")
+
+
+temperature_unit = input(f"Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+if user_temperature.isdigit():
+    
+    user_temperature = float(user_temperature)
+
+    if temperature_unit.upper() == "F":
+        
+        celsius = convert_to_celsius(user_temperature)
+        
+        print(f"{user_temperature}°F is {celsius}°C")
+        
+    elif temperature_unit.upper() == "C":
+        
+        fahrenheit = convert_to_fahrenheit(user_temperature)
+        
+        print(f"{user_temperature}°C is {fahrenheit}°F")
+        
+    else:
+        print(f"{temperature_unit} unit is not valid")
+        
+else:
+    print(f"Please enetr a number")
+    
+
+
