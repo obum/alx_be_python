27f6ad724811da18d3c874491721348dacc01035
obum <obumanichebe@gmail.com>
2024-09-24 11:27:29 +0100

    update: modified the shopping_list_manager file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,7 +15,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            new_item = input("Enter the item to add:")
+            new_item = input("Enter the item to add: ")
             shopping_list.append(new_item)
 
         elif choice == '2':
