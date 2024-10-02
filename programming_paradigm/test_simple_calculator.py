aeb4fab7dc5c6adec23c0338c4696ae036f325b5
obum <obumanichebe@gmail.com>
2024-10-02 13:57:49 +0100

    docs: Added the test codes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,7 +20,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(1, -1), 2)
         self.assertEqual(self.calc.subtract(1, 3), -2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(2, -3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
@@ -30,7 +30,7 @@ class TestSimpleCalculator(unittest.TestCase):
 
         
     
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         self.assertEqual(self.calc.divide(5, 0), None)
