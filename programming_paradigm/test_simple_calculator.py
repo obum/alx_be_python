693b54680514460a0388886a8aafdc6fde73b59c
obum <obumanichebe@gmail.com>
2024-10-02 13:55:30 +0100

    docs: Added the test codes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,34 +6,34 @@ class TestSimpleCalculator(unittest.TestCase):
     
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
-        self.a_calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
     
     def test_addition(self):
-        self.assertEqual(self.a_calculator.add(1, 1), 2)
-        self.assertEqual(self.a_calculator.add(-1, 1), 0)
-        self.assertEqual(self.a_calculator.add(-1, -1), -2)
-        self.assertEqual(self.a_calculator.add(1, -5), -4)
+        self.assertEqual(self.calc.add(1, 1), 2)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(1, -5), -4)
     
     def test_subtract(self):
-        self.assertEqual(self.a_calculator.subtract(1, 1), 0)
-        self.assertEqual(self.a_calculator.subtract(-1, -1), 0)
-        self.assertEqual(self.a_calculator.subtract(1, -1), 2)
-        self.assertEqual(self.a_calculator.subtract(1, 3), -2)
+        self.assertEqual(self.calc.subtract(1, 1), 0)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(1, -1), 2)
+        self.assertEqual(self.calc.subtract(1, 3), -2)
 
     def test_multiply(self):
-        self.assertEqual(self.a_calculator.multiply(2, 3), 6)
-        self.assertEqual(self.a_calculator.multiply(2, -3), -6)
-        self.assertEqual(self.a_calculator.multiply(-2, -3), 6)
-        self.assertEqual(self.a_calculator.multiply(2, 0), 0)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, -3), -6)
+        self.assertEqual(self.calc.multiply(-2, -3), 6)
+        self.assertEqual(self.calc.multiply(2, 0), 0)
         with self.assertRaises(TypeError):
-            self.a_calculator.divide(10, "str") 
+            self.calc.divide(10, "str") 
 
         
     
     def test_divide(self):
-        self.assertEqual(self.a_calculator.divide(4, 2), 2)
-        self.assertEqual(self.a_calculator.divide(5, 2), 2.5)
-        self.assertEqual(self.a_calculator.divide(5, 0), None)
-        self.assertEqual(self.a_calculator.divide(5, -2), -2.5)
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+        self.assertEqual(self.calc.divide(5, 0), None)
+        self.assertEqual(self.calc.divide(5, -2), -2.5)
         
-        self.assertRaises(TypeError, self.a_calculator.divide, "string", "str")
+        self.assertRaises(TypeError, self.calc.divide, "string", "str")
