2478d99caf06fcbfe40c0b08febbec55fc584d39
obum <obumanichebe@gmail.com>
2024-10-02 11:37:52 +0100

    Update: added the code for the bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount():
             return False
     
     def display_balance(self):
-        print(f'BankAccount: \n Current Balance: ${self.account_balance}')
+        print(f'Current Balance: ${self.account_balance}')
