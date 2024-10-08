cdc2f3b7bc681145b1f392d8039b9e51bb002bfb
obum <obumanichebe@gmail.com>
2024-10-08 16:05:01 +0100

    Docs: Added the polymorphism file

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -21,17 +21,17 @@ class Circle(Shape):
 
         
     def area(self):
-        return pi * self.radius * self.radius
+        return pi * self.radius**2
     
     
-def main():
-    shapes: list[Shape] = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+# def main():
+#     shapes: list[Shape] = [
+#         Rectangle(10, 5),
+#         Circle(7)
+#     ]
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+#     for shape in shapes:
+#         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
