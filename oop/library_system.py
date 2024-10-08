f28d24c546dba0957d2593f40be7d92ec0a05062
obum <obumanichebe@gmail.com>
2024-10-08 15:42:50 +0100

    Docs: Added code for the library-system file

--- oop/library_system.py
+++ oop/library_system.py
@@ -27,7 +27,7 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self):
-        self.books:list[Book] = []
+        self.books = []
         
         
     def add_book(self, book:Book):
@@ -45,24 +45,3 @@ class Library:
             else:
                 print(book)
                 
-
-
-def main():
-    # Create a Library instance
-    my_library = Library()
-
-    # Create instances of each type of book
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-    # Add books to the library
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
-    # List all books in the library
-    my_library.list_books()
-
-if __name__ == "__main__":
-    main()
